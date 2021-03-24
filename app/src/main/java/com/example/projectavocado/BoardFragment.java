9a19521153b4dfa365ddf76686c4e67a13afda5e
Mirdan94 <akinovmirdan@gmail.com>
2021-03-24 22:55:22 +0600

    - implemented Dots library

--- app/src/main/java/com/example/projectavocado/BoardFragment.java
+++ app/src/main/java/com/example/projectavocado/BoardFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -21,10 +22,23 @@ public class BoardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_board, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imageView);
         TextView textView = view.findViewById(R.id.textTitle);
         int pos = getArguments().getInt("pos");
+        switch (pos){
+            case 0:
+                textView.setText("Добро пожаловать в наше приложение по выбору лучших напитков!");
+                break;
+            case 1:
+                textView.setText("Выбирайте из огромного списка, который будет у вас под рукой");
+                break;
+            case 2:
+                textView.setText("Делитесь приложением с друзьями и получайте скидки!");
+                break;
+        }
+
         return view;
     }
 }
